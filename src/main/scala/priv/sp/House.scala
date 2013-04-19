91af12966e7415c30f4ea6fd4938844e0279cf07
illim <illminouche@gmail.com>
2013-04-19 16:37:31 +0200

    add few pics

--- src/main/scala/priv/sp/House.scala
+++ src/main/scala/priv/sp/House.scala
@@ -37,13 +37,13 @@ class Houses extends HouseCardEffects {
   import GameCardEffect._
 
   val Fire = House("fire", List(
-    Creature("GoblinBerserker", Some(4), 16, "Every turn deals 2 damage to owner adjacent cards", spec = creature(OnTurn ->goblinBerserker)),
+    Creature("Goblin", Some(4), 16, "Every turn deals 2 damage to owner adjacent cards", spec = creature(OnTurn ->goblinBerserker)),
     Creature("WallofFire", Some(0), 5, "Deals 5 damage to opponent creatures when summoned", spec = creature(Direct -> damageCreatures(Damage(5, isAbility = true)))),
-    Creature("PriestOfFire", Some(3), 13, "Every turn increase fire mana growth by 1", spec = creature(OnTurn -> addMana(1, 0)), isFocusable = false),
+    Creature("FireMonk", Some(3), 13, "Every turn increase fire mana growth by 1", spec = creature(OnTurn -> addMana(1, 0)), isFocusable = false),
     new FireDrake,
     Creature("OrcChieftain", Some(3), 16, "Increase attack of adjacent card by 2", boardEffect = Some(AddAttack(2, around = true))),
     Spell("FlameWave", "Deals 9 damage to opponent creatures", spec = spell(Direct -> damageCreatures(Damage(9, isSpell = true)))),
-    Creature("MinotaurCommander", Some(6), 20, "Increase attack of owner card by 1", boardEffect = Some(AddAttack(1))),
+    Creature("BullCommander", Some(6), 20, "Increase attack of owner card by 1", boardEffect = Some(AddAttack(1))),
     Creature("Bargul", Some(8), 25, "Deals 4 damage to every creature when summoned", spec = creature(Direct -> massDamage(Damage(4, isAbility = true)))),
     Spell("Inferno", "Deals 18 damage to target and 10 to other opponent creatures", inputSpec = Some(SelectTargetCreature), spec = spell(Direct -> inferno)),
     Creature("FireElemental", None, 36, "Fire Elemental deals 3 damage to opponent creatures when summoned", spec = creature(Direct -> damageCreatures(Damage(3, isAbility = true)), OnTurn -> addMana(1, 0))),
@@ -51,7 +51,7 @@ class Houses extends HouseCardEffects {
     Creature("Dragon", Some(9), 41, "Increase spell damage by 50%", mod = Some(new SpellMod(x => math.ceil(x * 1.5).intValue)))))
 
   val Water = House("water", List(
-    Spell("Meditation", "Increase normal mana by 1", spec = spell(Direct -> addMana(1, 0, 2, 3))),
+    Spell("Meditate", "Increase normal mana by 1", spec = spell(Direct -> addMana(1, 0, 2, 3))),
     Creature("seasprite", Some(5), 22, "Every turn deals 2 damage to owner",
       spec = creature(OnTurn -> { env : Env => env.player.life.%==( _ - env.guardSelf(2)) })),
     Creature("MerfolkApostate", Some(3), 10, "Give 2 fire mana when summoned", spec = creature(Direct -> addMana(2, 0))),
@@ -83,13 +83,13 @@ class Houses extends HouseCardEffects {
         env.player.life.%==( _ + math.min(env.getMana(3), 10))
       })),
     Creature("WallOfLightning", Some(0), 28, "Every turn deals 4 damage to opponent", spec = creature(OnTurn -> damage(Damage(4, isAbility = true)))),
-    Spell("LightningBolt", "Deals (5 + air power) damage to opponent",
+    Spell("Lightnin", "Deals (5 + air power) damage to opponent",
       spec = spell(Direct -> { env : Env => env.otherPlayer.life.%==( _ - env.guard(env.mod(Damage(5 + env.getMana(2), isSpell = true)).amount))})),
     Creature("Phoenix", Some(6), 18, "Can reborn if fire mana >= 10", boardEffect = Some(Reborn{ p =>
       p.houses(0).mana > 9
     })),
     Spell("ChainLightning", "Deals 9 damage to opponent and his creatures", spec = spell(Direct -> damageCreatures(Damage(9, isSpell = true)), Direct -> damage(Damage(9, isSpell = true)))),
-    Creature("LightningCloud", Some(4), 20, "Attack all opponent creatures", multipleTarget = true),
+    Creature("Cloud", Some(4), 20, "Attack all opponent creatures", multipleTarget = true),
     Spell("Tornado", "destroy target",
       inputSpec = Some(SelectTargetCreature),
       spec = spell(Direct -> { env: Env => env.otherPlayer.slots.%==( _ - env.selected) })),
@@ -107,7 +107,7 @@ class Houses extends HouseCardEffects {
       spec = spell(Direct -> { env : Env => env.player.life.%==( _ + 2 * env.getMana(3))})),
     Creature("elfHermit", Some(1), 13, "Increase earth mana growth by 2", spec = creature(OnTurn -> addMana(2, 3)), isFocusable = false),
     Spell("NaturesFury", "Deals to opponent the sum of the attacks of the 2 strongest owner creatures", spec = spell(Direct -> fury)),
-    Creature("GiantSpider", Some(4), 21, "Spawn 2 forest spiders around him", spec = creature(Direct -> spider)),
+    Creature("HugeSpider", Some(4), 21, "Spawn 2 forest spiders around him", spec = creature(Direct -> spider)),
     Creature("Troll", Some(6), 25, "Every turn heals himself by 4", spec = creature(OnTurn -> healCreature(4))),
     Spell("StoneRain", "Deals 25 damage to any creature", spec = spell(Direct -> massDamage(Damage(25, isSpell = true)))),
     Creature("EarthElemental", None, 49, spec = creature(OnTurn -> addMana(1, 2))),
