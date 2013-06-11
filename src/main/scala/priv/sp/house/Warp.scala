b9faac56b31d4b29558642ff0214f332b0a8db1d
illim <illminouche@gmail.com>
2013-06-11 13:00:04 +0200

    nothing

--- src/main/scala/priv/sp/house/Warp.scala
+++ src/main/scala/priv/sp/house/Warp.scala
@@ -5,6 +5,8 @@ import priv.sp.update._
 import CardSpec._
 import GameCardEffect._
 
+// FIXME: stranger bugged with house with specific listener
+// schizo when unbridle
 class Warp {
 
   val Warp = House("Warp", List(
@@ -12,9 +14,9 @@ class Warp {
     Spell("EarthQuake", "Deals to opponent creatures damage equals to their mana", effects = effects(Direct -> quake)),
     Creature("Cloak", Attack(4), 18, "When die restore the creature.(Can't cloak himself)", inputSpec = Some(SelectOwnerCreature), reaction = new CloakReaction),
     Creature("Photographer", Attack(3), 18, "If there's already a photograph, owner slots is reverted to the state\nwhen last one was spawned", effects = effects(Direct -> photo)),
-    Creature("WatchMaker", Attack(6), 25, "When summoned, opposite opposite creature lose his abilities\nuntil watchmaker die.", reaction = new WMReaction),
+    Creature("Schizo", Attack(6), 25, "When summoned, opposite opposite creature lose his abilities\nuntil schizo die.", reaction = new SchizoReaction),
     Creature("Ram", Attack(6), 26, "Opposite creature is destroyed and opponent get his mana back -2.", effects = effects(Direct -> ram)),
-    Creature("Stranger", AttackSources().add(new StrangerAttack), 35, "Attack is highest opponent mana.\nWhen summoned, take effects of opposite slot.", effects = effects(Direct -> merge)),
+    Creature("Stranger", AttackSources().add(new StrangerAttack), 35, "Attack is highest opponent mana.\nWhen summoned, take effects of opposite slot.\n(not working with all special class)", effects = effects(Direct -> merge)),
     Creature("WarpQueen", Attack(6), 35, "Opponent creatures lose their ability until end of next turn.\nDeals 5 damage to each of them", effects = effects(Direct -> warp))))
 
   val photographer = Warp.cards(3)
@@ -83,8 +85,8 @@ class Warp {
       }
     }
   }
-  class WMReaction extends Reaction {
-    // bugged when bridled, unbridle shouldn't bridle opposite slot?
+  class SchizoReaction extends Reaction {
+
     override def onAdd(selected : Int, slot : SlotUpdate) {
       if (selected == slot.num){
         val oppSlot = slot.slots.player.otherPlayer.slots(selected)
@@ -183,4 +185,6 @@ extends Creature(
   houseIndex = s.houseIndex
   cost = s.cost
   id = s.id
+
+  final override def inflict(damage : Damage, life : Int) = c.inflict(damage, life)
 }
